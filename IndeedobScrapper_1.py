37eee397e516278d7d04d6dfe637cda6fafe38cc
MirzaHanan <Mirzahh07@gmail.com>
2022-07-25 18:16:36 +0500

    Starts saving data in csv file

--- IndeedobScrapper_1.py
+++ IndeedobScrapper_1.py
@@ -1,3 +1,6 @@
+from csv import writer
+import csv
+from tkinter.messagebox import NO
 import requests
 from bs4 import BeautifulSoup
 
@@ -19,34 +22,49 @@ try:
 
     jobSearchResults = soup.find( "ul" , class_="jobsearch-ResultsList css-0")
     # print(jobSearchResults.prettify())
+    field_name = ["Job Title" , "Company Name" , "Location"]
 
-    for jobSearchResult in jobSearchResults:
+    with open("job.csv","w" , newline="")as f:
+        csv_writer = csv.writer(f, delimiter=",")
+        csv_writer.writerow(field_name)
 
-        print()
-
-        title = jobSearchResult.find("h2" , class_="jobTitle")
-        # print( " Title Type : ",type(title))
-        # print(title)
-        if title == None:
-            continue
-        for item in title:
-            job_title = item.find("span")
-            if job_title == None:
+        for jobSearchResult in jobSearchResults:
+            print()
+            title = jobSearchResult.find("h2" , class_="jobTitle")
+            if title == None:
                 continue
-            print(job_title.text)
+            for item in title:
+                job_title = item.find("span")
+                if job_title == None:
+                    pass
+                else:
+                    print(job_title.text)
+            companyName = jobSearchResult.find("a" , class_="companyOverviewLink" )
+            if companyName == None:
+                pass
+            else:
+                print(companyName.text)
 
-        # class="turnstileLink companyOverviewLink"
-        companyName = jobSearchResult.find("a" , class_="companyOverviewLink" )
-        if companyName == None:
-            continue
-        print(companyName.text)
+            companyLocation = jobSearchResult.find("div" , class_="companyLocation")
+            if companyLocation == None:
+                pass
+            else:
+                print(companyLocation.text)
+            if job_title != None and companyName != None and companyLocation != None:
+                csv_writer.writerow([job_title.text , companyName.text , companyLocation.text])
+            elif job_title == None:
+                csv_writer.writerow([None , companyName.text , companyLocation.text])
+            elif companyName == None:
+                csv_writer.writerow([job_title.text , None , companyLocation.text])
+            elif companyLocation == None:
+                csv_writer.writerow([job_title.text , companyName.text , None])
+            else:
+                csv_writer.writerow([])
+            
 
-        # class="companyLocation"
 
-        companyLocation = jobSearchResult.find("div" , class_="companyLocation")
-        if companyLocation == None:
-            continue
-        print(companyLocation.text)
+
+
 
 
 
